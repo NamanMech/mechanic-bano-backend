523b55fd11be5335272fe4a32015aef4747ea4de
NamanMech <mechanicseekho@gmail.com>
2025-06-24 21:06:07 +0530

    Update index.js

--- api/tutorials/index.js
+++ api/tutorials/index.js
@@ -1,7 +1,21 @@
 import dbConnect from '../../utils/dbConnect.js';
 import Tutorial from '../../models/Tutorial.js';
 
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   await dbConnect();
 
   if (req.method === 'GET') {
@@ -17,6 +31,7 @@ export default async function handler(req, res) {
       await tutorial.save();
       res.status(201).json(tutorial);
     } catch (error) {
+      console.error('POST error:', error);
       res.status(500).json({ message: 'Error adding tutorial' });
     }
   } else {
