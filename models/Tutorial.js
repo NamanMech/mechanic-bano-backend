269b4f46d6fce89976c2a134cee9bbf656f0c1de
NamanMech <mechanicseekho@gmail.com>
2025-06-24 19:43:42 +0530

    Create Tutorial.js

---
+++ models/Tutorial.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const TutorialSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  youtubeLink: String,
+});
+
+export default mongoose.models.Tutorial || mongoose.model('Tutorial', TutorialSchema);
