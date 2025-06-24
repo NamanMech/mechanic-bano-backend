f3222580cb5f7768811fe6d8ddb9c502fdc7de7c
NamanMech <mechanicseekho@gmail.com>
2025-06-24 19:41:46 +0530

    Create index.js

---
+++ api/tutorials/index.js
@@ -0,0 +1,25 @@
+import dbConnect from '../../../utils/dbConnect';
+import Tutorial from '../../../models/Tutorial';
+
+export default async function handler(req, res) {
+  await dbConnect();
+
+  if (req.method === 'GET') {
+    try {
+      const tutorials = await Tutorial.find();
+      res.status(200).json(tutorials);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching tutorials' });
+    }
+  } else if (req.method === 'POST') {
+    try {
+      const tutorial = new Tutorial(req.body);
+      await tutorial.save();
+      res.status(201).json(tutorial);
+    } catch (error) {
+      res.status(500).json({ message: 'Error adding tutorial' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
