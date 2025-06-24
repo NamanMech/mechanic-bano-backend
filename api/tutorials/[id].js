ee216ae1caab55901c9d72cab2e6cb52c1d7b654
NamanMech <mechanicseekho@gmail.com>
2025-06-24 21:07:04 +0530

    Update [id].js

--- api/tutorials/[id].js
+++ api/tutorials/[id].js
@@ -1,7 +1,21 @@
 import dbConnect from '../../utils/dbConnect.js';
 import Tutorial from '../../models/Tutorial.js';
 
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   await dbConnect();
   const { id } = req.query;
 
