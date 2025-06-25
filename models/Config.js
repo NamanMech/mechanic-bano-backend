f0d2f6fe9e0c3b402e3469f0057f88b0d499c904
NamanMech <mechanicseekho@gmail.com>
2025-06-26 04:50:50 +0530

    Update

--- models/Config.js
+++ models/Config.js
@@ -1,1 +1,9 @@
 
+import mongoose from 'mongoose';
+
+const ConfigSchema = new mongoose.Schema({
+  siteName: String,
+  logoUrl: String
+});
+
+export default mongoose.models.Config || mongoose.model('Config', ConfigSchema);
--- models/Tutorial.js
+++ models/Tutorial.js
@@ -1,3 +1,4 @@
+
 import mongoose from 'mongoose';
 
 const TutorialSchema = new mongoose.Schema({
