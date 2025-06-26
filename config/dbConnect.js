ed07a7634fbb1aca5ba84002781aa3a88e888c67
NamanMech <mechanicseekho@gmail.com>
2025-06-26 18:41:10 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,14 +1,14 @@
+// config/dbConnect.js
 import mongoose from 'mongoose';
 
 const dbConnect = async () => {
   if (mongoose.connections[0].readyState) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected successfully.');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    throw new Error('MongoDB connection failed.');
   }
 };
 
