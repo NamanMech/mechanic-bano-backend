44f14289b6532cf78b879ceb34fcf0274ede589f
NamanMech <mechanicseekho@gmail.com>
2025-06-26 16:09:20 +0530

    Update and rename dbConnect.js to dbConnect.js

---
+++ config/dbConnect.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const dbConnect = async () => {
+  if (mongoose.connections[0].readyState) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected successfully.');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw new Error('MongoDB connection failed.');
+  }
+};
+
+export default dbConnect;
