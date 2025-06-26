2bb7c550507ea8d03cce936cec5f924571635566
NamanMech <mechanicseekho@gmail.com>
2025-06-26 20:37:00 +0530

    Update Tutorial.js

--- models/Tutorial.js
+++ models/Tutorial.js
@@ -1,12 +1,9 @@
-// models/Tutorial.js
 import mongoose from 'mongoose';
 
-const tutorialSchema = new mongoose.Schema({
+const TutorialSchema = new mongoose.Schema({
   title: String,
   description: String,
   youtubeLink: String,
 });
 
-const Tutorial = mongoose.models.Tutorial || mongoose.model('Tutorial', tutorialSchema);
-
-export default Tutorial;
+export default mongoose.models.Tutorial || mongoose.model('Tutorial', TutorialSchema);
