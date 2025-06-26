e6abf5e646b1529be58da0fee058e1702dbe49d3
NamanMech <mechanicseekho@gmail.com>
2025-06-26 16:14:52 +0530

    Update index.js

--- api/config/index.js
+++ api/config/index.js
@@ -1,5 +1,4 @@
-
-import dbConnect from '../../utils/dbConnect';
+import dbConnect from '../../config/dbConnect';
 import Config from '../../models/Config';
 
 export default async function handler(req, res) {
@@ -7,26 +6,30 @@ export default async function handler(req, res) {
 
   if (req.method === 'GET') {
     try {
-      const config = await Config.findOne();
+      let config = await Config.findOne();
+      if (!config) {
+        config = new Config({ websiteName: 'Mechanic Bano', logoURL: '' });
+        await config.save();
+      }
       res.status(200).json(config);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching config' });
     }
-  } else if (req.method === 'POST') {
+  } else if (req.method === 'PUT') {
     try {
-      const { siteName, logoUrl } = req.body;
+      const { websiteName, logoURL } = req.body;
       let config = await Config.findOne();
 
       if (config) {
-        config.siteName = siteName;
-        config.logoUrl = logoUrl;
+        config.websiteName = websiteName;
+        config.logoURL = logoURL;
         await config.save();
       } else {
-        config = new Config({ siteName, logoUrl });
+        config = new Config({ websiteName, logoURL });
         await config.save();
       }
 
-      res.status(201).json(config);
+      res.status(200).json({ message: 'Config updated successfully' });
     } catch (error) {
       res.status(500).json({ message: 'Error updating config' });
     }
