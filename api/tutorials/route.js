20aea2147588ad5d21f1bd05231e7362409aad36
NamanMech <mechanicseekho@gmail.com>
2025-06-26 18:45:31 +0530

    Create route.js

---
+++ api/tutorials/route.js
@@ -0,0 +1,19 @@
+// api/tutorials/route.js
+import dbConnect from '../../config/dbConnect';
+import Tutorial from '../../../models/Tutorial';
+
+export const GET = async () => {
+  await dbConnect();
+  const tutorials = await Tutorial.find();
+  return new Response(JSON.stringify(tutorials), { status: 200 });
+};
+
+export const POST = async (req) => {
+  await dbConnect();
+  const { title, description, youtubeLink } = await req.json();
+
+  const tutorial = new Tutorial({ title, description, youtubeLink });
+  await tutorial.save();
+
+  return new Response(JSON.stringify({ message: 'Tutorial added successfully' }), { status: 201 });
+};
