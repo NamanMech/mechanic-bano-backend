4f7ccd136487d2f98129ec4e64ccf529061d0997
NamanMech <mechanicseekho@gmail.com>
2025-06-28 18:28:02 +0530

    Update route.js

--- api/config/route.js
+++ api/config/route.js
@@ -1,19 +1,22 @@
 // api/config/route.js
+
 import dbConnect from '../../config/dbConnect';
 import Config from '../../models/Config';
 
+// GET CONFIG
 export const GET = async () => {
   await dbConnect();
   let config = await Config.findOne();
 
   if (!config) {
-    config = new Config({ websiteName: 'Mechanic Bano' });
+    config = new Config({ websiteName: 'Mechanic Bano', logoURL: '' });
     await config.save();
   }
 
   return new Response(JSON.stringify(config), { status: 200 });
 };
 
+// UPDATE CONFIG
 export const PUT = async (req) => {
   await dbConnect();
   const { websiteName } = await req.json();
@@ -23,7 +26,7 @@ export const PUT = async (req) => {
     config.websiteName = websiteName;
     await config.save();
   } else {
-    config = new Config({ websiteName });
+    config = new Config({ websiteName, logoURL: '' });
     await config.save();
   }
 
