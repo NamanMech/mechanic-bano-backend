13cf24a96f41424f4d92a58642712b886d536237
NamanMech <mechanicseekho@gmail.com>
2025-06-28 18:10:14 +0530

    Delete models/Tutorial.js

--- models/Tutorial.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const TutorialSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  youtubeLink: String,
-});
-
-export default mongoose.models.Tutorial || mongoose.model('Tutorial', TutorialSchema);
