432416f3237514678ef56585dd62d122614439e9
NamanMech <mechanicseekho@gmail.com>
2025-06-29 01:46:15 +0530

    Utils db

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,1 +1,12 @@
+import { MongoClient } from "mongodb";
 
+let cachedClient = null;
+
+export async function connectDB() {
+  if (cachedClient) return cachedClient;
+
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  cachedClient = client;
+  return client;
+}
