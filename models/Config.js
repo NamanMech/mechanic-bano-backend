a8d7f587fcfe8068b9c8c4ce3e579c9aee661582
NamanMech <mechanicseekho@gmail.com>
2025-06-29 01:33:34 +0530

    Delete models directory

--- models/Config.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const ConfigSchema = new mongoose.Schema({
-  websiteName: { type: String, required: true },
-});
-
-export default mongoose.models.Config || mongoose.model('Config', ConfigSchema);
