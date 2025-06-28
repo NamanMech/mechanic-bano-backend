26da109424136457b2ef702fba26c3ff001049d4
NamanMech <mechanicseekho@gmail.com>
2025-06-28 18:42:19 +0530

    Update Config.js

--- models/Config.js
+++ models/Config.js
@@ -1,10 +1,7 @@
-// models/Config.js
-
 import mongoose from 'mongoose';
 
 const ConfigSchema = new mongoose.Schema({
   websiteName: { type: String, required: true },
-  logoURL: { type: String, default: '' }
 });
 
 export default mongoose.models.Config || mongoose.model('Config', ConfigSchema);
