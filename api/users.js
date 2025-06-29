3b974ac095088c1d50cc6d1a7f88760e85db38fc
NamanMech <mechanicseekho@gmail.com>
2025-06-29 21:35:47 +0530

    Update and rename user.js to users.js

---
+++ api/users.js
@@ -0,0 +1,13 @@
+// api/users.js
+const { connectDB } = require('../utils/connectDB');
+
+module.exports = async (req, res) => {
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
+  const collection = db.collection('users');
+
+  if (req.method === 'GET') {
+    const users = await collection.find().toArray();
+    return res.status(200).json(users);
+  }
+};
