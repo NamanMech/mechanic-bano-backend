779dd804cc11f4dcbb96b1e44d2895925ae95c34
NamanMech <mechanicseekho@gmail.com>
2025-06-30 05:15:43 +0530

    Update youtube.js

--- api/youtube.js
+++ api/youtube.js
@@ -1,33 +1,28 @@
-const { connectDB } = require('../utils/connectDB.js');
-const { ObjectId } = require('mongodb');
+// api/youtube.js
+import { connectDB } from '../utils/connectDB.js';
+import { ObjectId } from 'mongodb';
 
-module.exports = async (req, res) => {
-  try {
-    const client = await connectDB();
-    const db = client.db('mechanic_bano');
-    const collection = db.collection('youtube_videos');
+export default async function handler(req, res) {
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
+  const collection = db.collection('youtube_videos');
 
-    if (req.method === 'GET') {
-      const videos = await collection.find().toArray();
-      return res.status(200).json(videos);
-    }
-
-    if (req.method === 'POST') {
-      const { title, description, link } = req.body;
-
-      await collection.insertOne({ title, description, link });
-      return res.status(201).json({ message: 'YouTube video added successfully' });
-    }
-
-    if (req.method === 'DELETE') {
-      const { id } = req.query;
-      await collection.deleteOne({ _id: new ObjectId(id) });
-      return res.status(200).json({ message: 'Video deleted successfully' });
-    }
-
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  } catch (error) {
-    console.error('API Error:', error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+  if (req.method === 'GET') {
+    const videos = await collection.find().toArray();
+    return res.status(200).json(videos);
   }
-};
+
+  if (req.method === 'POST') {
+    const { title, description, link } = req.body;
+    await collection.insertOne({ title, description, link });
+    return res.status(201).json({ message: 'YouTube video added successfully' });
+  }
+
+  if (req.method === 'DELETE') {
+    const { id } = req.query;
+    await collection.deleteOne({ _id: new ObjectId(id) });
+    return res.status(200).json({ message: 'Video deleted successfully' });
+  }
+
+  return res.status(405).json({ message: 'Method Not Allowed' });
+}
