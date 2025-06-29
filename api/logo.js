ca268891acb2d8cb82eb6a4820dd75359ffa65c9
NamanMech <mechanicseekho@gmail.com>
2025-06-29 21:27:03 +0530

    Update logo.js

--- api/logo.js
+++ api/logo.js
@@ -1,18 +1,24 @@
-import { connectDB } from '../utils/connectDB';
+// api/logo.js
+const { connectDB } = require('../utils/connectDB');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const client = await connectDB();
   const db = client.db('mechanic_bano');
-  const collection = db.collection('site_config');
+  const collection = db.collection('logo');
 
   if (req.method === 'GET') {
-    const config = await collection.findOne({});
-    return res.json(config);
+    const logoData = await collection.find().toArray();
+    return res.status(200).json(logoData);
   }
 
   if (req.method === 'POST') {
-    const { siteName, logoUrl } = req.body;
-    await collection.updateOne({}, { $set: { siteName, logoUrl } }, { upsert: true });
-    return res.json({ message: 'Updated successfully' });
+    let body = '';
+    req.on('data', chunk => { body += chunk.toString(); });
+    req.on('end', async () => {
+      const { siteName, logoUrl } = JSON.parse(body);
+      await collection.deleteMany({}); // Replace existing logo
+      await collection.insertOne({ siteName, logoUrl });
+      return res.status(201).json({ message: 'Logo and site name updated successfully' });
+    });
   }
-}
+};
