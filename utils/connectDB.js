598bfbd65965aeb3647f01bda3938e64bfcb6589
NamanMech <mechanicseekho@gmail.com>
2025-06-29 22:55:01 +0530

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,16 +1,14 @@
 // utils/connectDB.js
-const { MongoClient } = require('mongodb');
+
+import { MongoClient } from "mongodb";
 
 let cachedClient = null;
 
-async function connectDB() {
+export async function connectDB() {
   if (cachedClient) return cachedClient;
 
   const client = new MongoClient(process.env.MONGO_URI);
   await client.connect();
   cachedClient = client;
-
   return client;
 }
-
-module.exports = { connectDB };
