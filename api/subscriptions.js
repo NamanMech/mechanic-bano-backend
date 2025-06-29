af763e48bc663e9484689e3edba0fdb3b582627b
NamanMech <mechanicseekho@gmail.com>
2025-06-29 21:34:11 +0530

    Update and rename subscription.js to subscriptions.js

---
+++ api/subscriptions.js
@@ -0,0 +1,30 @@
+// api/subscriptions.js
+const { connectDB } = require('../utils/connectDB');
+const { ObjectId } = require('mongodb');
+
+module.exports = async (req, res) => {
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
+  const collection = db.collection('subscriptions');
+
+  if (req.method === 'GET') {
+    const subscriptions = await collection.find().toArray();
+    return res.status(200).json(subscriptions);
+  }
+
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => { body += chunk.toString(); });
+    req.on('end', async () => {
+      const { subscriptionName, price } = JSON.parse(body);
+      await collection.insertOne({ subscriptionName, price });
+      return res.status(201).json({ message: 'Subscription added successfully' });
+    });
+  }
+
+  if (req.method === 'DELETE') {
+    const { id } = req.query;
+    await collection.deleteOne({ _id: new ObjectId(id) });
+    return res.status(200).json({ message: 'Subscription deleted successfully' });
+  }
+};
