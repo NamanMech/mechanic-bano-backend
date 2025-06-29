126d63ff60efb551ae1d799193abab873941db8e
NamanMech <mechanicseekho@gmail.com>
2025-06-29 22:58:27 +0530

    Update categories.js

--- api/categories.js
+++ api/categories.js
@@ -1,30 +1,31 @@
-// api/categories.js
-const { connectDB } = require('../utils/connectDB');
-const { ObjectId } = require('mongodb');
+import { connectDB } from '../utils/connectDB';
+import { ObjectId } from 'mongodb';
 
-module.exports = async (req, res) => {
-  const client = await connectDB();
-  const db = client.db('mechanic_bano');
-  const collection = db.collection('categories');
+export default async function handler(req, res) {
+  try {
+    const client = await connectDB();
+    const db = client.db('mechanic_bano');
+    const collection = db.collection('categories');
 
-  if (req.method === 'GET') {
-    const categories = await collection.find().toArray();
-    return res.status(200).json(categories);
-  }
+    if (req.method === 'GET') {
+      const categories = await collection.find().toArray();
+      return res.status(200).json(categories);
+    }
 
-  if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => { body += chunk.toString(); });
-    req.on('end', async () => {
-      const { name, description } = JSON.parse(body);
+    if (req.method === 'POST') {
+      const { name, description } = req.body;
       await collection.insertOne({ name, description });
       return res.status(201).json({ message: 'Category added successfully' });
-    });
-  }
+    }
 
-  if (req.method === 'DELETE') {
-    const { id } = req.query;
-    await collection.deleteOne({ _id: new ObjectId(id) });
-    return res.status(200).json({ message: 'Category deleted successfully' });
+    if (req.method === 'DELETE') {
+      const { id } = req.query;
+      await collection.deleteOne({ _id: new ObjectId(id) });
+      return res.status(200).json({ message: 'Category deleted successfully' });
+    }
+
+    return res.status(405).json({ message: 'Method not allowed' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server Error', error: error.message });
   }
-};
+}
