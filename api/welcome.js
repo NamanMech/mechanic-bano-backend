02328524a40e24d97eaba4b76dab60b1a3104156
NamanMech <mechanicseekho@gmail.com>
2025-06-30 05:16:22 +0530

    Update welcome.js

--- api/welcome.js
+++ api/welcome.js
@@ -1,27 +1,22 @@
-const { connectDB } = require('../utils/connectDB.js');
+// api/welcome.js
+import { connectDB } from '../utils/connectDB.js';
 
-module.exports = async (req, res) => {
-  try {
-    const client = await connectDB();
-    const db = client.db('mechanic_bano');
-    const collection = db.collection('welcome_note');
+export default async function handler(req, res) {
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
+  const collection = db.collection('welcome_note');
 
-    if (req.method === 'GET') {
-      const notes = await collection.find().toArray();
-      return res.status(200).json(notes);
-    }
-
-    if (req.method === 'POST') {
-      const { note } = req.body;
-
-      await collection.deleteMany({});
-      await collection.insertOne({ note });
-      return res.status(201).json({ message: 'Welcome note added successfully' });
-    }
-
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  } catch (error) {
-    console.error('API Error:', error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+  if (req.method === 'GET') {
+    const notes = await collection.find().toArray();
+    return res.status(200).json(notes);
   }
-};
+
+  if (req.method === 'POST') {
+    const { note } = req.body;
+    await collection.deleteMany({});
+    await collection.insertOne({ note });
+    return res.status(201).json({ message: 'Welcome note added successfully' });
+  }
+
+  return res.status(405).json({ message: 'Method Not Allowed' });
+}
