b9aef68af87c5833970e090636ea4b2ebac410e8
NamanMech <mechanicseekho@gmail.com>
2025-06-29 21:28:49 +0530

    Update and rename welcomeNote.js to welcome.js

---
+++ api/welcome.js
@@ -0,0 +1,24 @@
+// api/welcome.js
+const { connectDB } = require('../utils/connectDB');
+
+module.exports = async (req, res) => {
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
+  const collection = db.collection('welcome_note');
+
+  if (req.method === 'GET') {
+    const notes = await collection.find().toArray();
+    return res.status(200).json(notes);
+  }
+
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => { body += chunk.toString(); });
+    req.on('end', async () => {
+      const { note } = JSON.parse(body);
+      await collection.deleteMany({}); // Replace existing welcome note
+      await collection.insertOne({ note });
+      return res.status(201).json({ message: 'Welcome note added successfully' });
+    });
+  }
+};
