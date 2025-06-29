a3558ec197e51baac937ed6213a0467ce221c0fb
NamanMech <mechanicseekho@gmail.com>
2025-06-30 05:17:02 +0530

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,19 +1,15 @@
-const { connectDB } = require('../utils/connectDB.js');
+// api/users.js
+import { connectDB } from '../utils/connectDB.js';
 
-module.exports = async (req, res) => {
-  try {
-    const client = await connectDB();
-    const db = client.db('mechanic_bano');
-    const collection = db.collection('users');
+export default async function handler(req, res) {
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
+  const collection = db.collection('users');
 
-    if (req.method === 'GET') {
-      const users = await collection.find().toArray();
-      return res.status(200).json(users);
-    }
-
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  } catch (error) {
-    console.error('API Error:', error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+  if (req.method === 'GET') {
+    const users = await collection.find().toArray();
+    return res.status(200).json(users);
   }
-};
+
+  return res.status(405).json({ message: 'Method Not Allowed' });
+}
