39fb11983e571d08d5242d2d4fb376ab8b21c5a4
NamanMech <mechanicseekho@gmail.com>
2025-07-06 22:48:15 +0530

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,13 +1,22 @@
 // utils/connectDB.js
 import { MongoClient } from 'mongodb';
 
-let cachedClient = null;
+let cached = global.mongo;
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null };
+}
 
 export async function connectDB() {
-  if (cachedClient) return cachedClient;
+  if (cached.conn) return cached.conn;
 
-  const client = new MongoClient(process.env.MONGO_URI);
-  await client.connect();
-  cachedClient = client;
-  return client;
+  if (!cached.promise) {
+    const client = new MongoClient(process.env.MONGO_URI);
+    cached.promise = client.connect().then((client) => {
+      return client;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
