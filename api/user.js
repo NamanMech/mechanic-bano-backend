43be2969e0daa8beb9f4523f1c907d2069d331a3
NamanMech <mechanicseekho@gmail.com>
2025-07-06 20:05:51 +0530

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,7 +1,18 @@
+// api/user.js
 import { connectDB } from '../utils/connectDB.js';
 
 export default async function handler(req, res) {
-  const db = await connectDB();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const client = await connectDB();
+  const db = client.db('mechanic_bano');
   const usersCollection = db.collection('users');
 
   if (req.method === 'GET') {
