f2b6f4435eb72a7262d6dc9dab5e44379b0f76d7
NamanMech <mechanicseekho@gmail.com>
2025-07-06 22:55:42 +0530

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,6 +1,12 @@
 // utils/connectDB.js
 import { MongoClient } from 'mongodb';
 
+const uri = process.env.MONGO_URI;
+
+if (!uri) {
+  throw new Error('Please define the MONGO_URI environment variable.');
+}
+
 let cached = global.mongo;
 
 if (!cached) {
@@ -8,10 +14,12 @@ if (!cached) {
 }
 
 export async function connectDB() {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
-    const client = new MongoClient(process.env.MONGO_URI);
+    const client = new MongoClient(uri);
     cached.promise = client.connect().then((client) => {
       return client;
     });
