e5842e91414749d31d03ae7d3bc900062936007e
NamanMech <mechanicseekho@gmail.com>
2025-09-03 16:14:42 +0530

    Update jsonParser.js

--- utils/jsonParser.js
+++ utils/jsonParser.js
@@ -1,5 +1,9 @@
 export function parseJsonBody(req) {
   return new Promise((resolve, reject) => {
+    if (req.body && typeof req.body === 'object' && Object.keys(req.body).length > 0) {
+      // If body is already parsed by platform, return it
+      return resolve(req.body);
+    }
     let body = '';
     req.on('data', chunk => { body += chunk.toString(); });
     req.on('end', () => {
