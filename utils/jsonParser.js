5b2a3bfc1f2a42279a855c5f8bef06a89dd8ac35
NamanMech <mechanicseekho@gmail.com>
2025-09-03 12:41:25 +0530

    Update and rename parseBody.js to jsonParser.js

--- utils/jsonParser.js
+++ utils/jsonParser.js
@@ -1,14 +1,14 @@
 export function parseJsonBody(req) {
   return new Promise((resolve, reject) => {
-    let body = "";
-    req.on("data", (chunk) => (body += chunk));
-    req.on("end", () => {
+    let body = '';
+    req.on('data', chunk => { body += chunk.toString(); });
+    req.on('end', () => {
       try {
         resolve(body ? JSON.parse(body) : {});
       } catch (err) {
         reject(new Error("Invalid JSON format"));
       }
     });
-    req.on("error", reject);
+    req.on('error', reject);
   });
 }
