7877279a0de1cac46702063b5424c0ecc163be99
NamanMech <mechanicseekho@gmail.com>
2025-09-03 11:24:42 +0530

    Create cors.js

---
+++ utils/cors.js
@@ -0,0 +1,21 @@
+// utils/cors.js
+const allowedOrigins = [
+  "https://mechanic-bano-admin.netlify.app",
+  "http://localhost:3000",
+  "http://localhost:5173"
+];
+
+export function setCorsHeaders(req, res) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+  );
+}
